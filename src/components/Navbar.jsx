cca9acc717b22321bf97012deee41bddd4bedbca
Tyler <nuvallo.inc@gmail.com>
2020-01-22 19:40:30 -0500

    navigation toggle working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React from "react";
+import useDarkMode from "../hooks/useDarkMode";
+
+const darkKey = true;
+const darkInitial = "isDM";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  const [darkMode, toggleDarkMode] = useDarkMode(darkKey, darkInitial);
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          onClick={toggleDarkMode}
+          className={darkMode ? "toggle toggled" : "toggle"}
         />
       </div>
     </nav>
